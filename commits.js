a658cd260adbbb8136630c71c1d57917ec82d57c
Zoltan Dezso <dezso.zoltan@gmail.com>
2012-09-25 10:36:57 +0900

    Refactor charts and add IM data

--- commits.js
+++ commits.js
@@ -1,343 +1,183 @@
 window.onload = function() {
-  var canvas  = document.getElementById("canvas");
-  var context = canvas.getContext('2d');
-
-  var width        = canvas.width;
-  var height       = canvas.height;
-  var day_w        = 4;
-  var hour_h       = 4;
-  var day_gap      = 1;
-  var hour_gap     = 1;
-  var colorIndex   = 0;
-  var startYear    = 2006;
-  var endYear      = 2013;
-  var startMonth   = 9;
-  var legendColor  = "#cccccc";
-
-  function daysSinceStart(date) {
-    var startDate = new Date("" + startYear + "-" + startMonth + "-01 00:00:00");
-    var days = parseInt((date - startDate) / 86400000);
-
-    return(days);
-  }
-
-  function coordForTime(days, hour, y) {
-    return [ days*(day_w + day_gap), y - hour*(hour_h + hour_gap) ];
-  }
-
-  function hourLine(hour, y) {
-    context.moveTo(0, y - hour*(hour_h + hour_gap));
-    context.lineTo(width, y - hour*(hour_h + hour_gap));
-  }
-
-  function dayLine(day, y, text, offset) {
-    if (typeof(day) === "string") {
-      day = new Date(day);
-    }
-    var days = daysSinceStart(day);
-
-    var pos  = coordForTime(days, 0, y);
-    if (typeof(text) === "string") {
-      context.font = "9px Helvetica";
-      context.fillText(text, pos[0], pos[1] + offset);
-    }
-
-    context.moveTo(pos[0], pos[1]);
-    pos  = coordForTime(days, 23, y);
-    context.lineTo(pos[0], pos[1]);
-
-  }
-
-  //{{{ - Drawing
-  function clear(y) {
-    context.fillStyle = "#ffffff";
-    context.fillRect(0, y - 160, width, y + 16);
-  }
-
-  function drawGrid(x, y) {
-    context.stokeStyle = "#ff0000";
-    context.fillStyle  = "#000000";
-    context.lineWidth = 0.1;
-    for(i=0;i<24;i++) {
-      hourLine(i, y);
-    }
-
-    for(i=startYear;i<endYear+1;i++) {
-      dayLine(""+(i) + "-01-01 00:00:00", y, ""+(i), 20);
-
-      for(j=1;j<13;j++) {
-        dayLine("" + (i) + "-" + j + "-01 00:00:00", y, ""+(j), 30);
-      }
-    }
-
-    // Custom hour markers
-    hourLine(9, y);
-    hourLine(12, y);
-    hourLine(18, y);
-    hourLine(20, y);
-
-    context.stroke();
-  }
-
-  function drawHeader(text, x, y) {
-    var font = context.font;
-    context.fillStyle = "#000000";
-    context.font = "32px Helvetica";
-    context.fillText(text, x, y);
-    context.font = font;
-  }
-
-  function drawLegendSmall(text, x, y) {
-    context.fillStyle = "#000000";
-    context.font = "8px Helvetica";
-    context.fillText(text, x, y);
-  }
-
-  function drawSummaries(summaries, y, factor) {
-    summaries[0].forEach(function (month) {
-      context.fillStyle = legendColor;
-      var val = month.value / factor;
-      context.fillRect(200+month.key*10+3, y - val, 9, val);
-      drawLegendSmall(""+(month.key+1), 200+month.key*10+3, y + 8);
-    });
-
-    summaries[1].forEach(function (day) {
-      context.fillStyle = legendColor;
-      var val = day.value / (factor / 3);
-      context.fillRect(350+day.key*10+3, y - val, 9, val);
-      drawLegendSmall(""+(day.key), 350+day.key*10+3, y + 8);
-    });
-    summaries[2].forEach(function (dow) {
-      context.fillStyle = legendColor;
-      var val = dow.value / factor;
-      var dows = ["S", "M", "T", "W", "T", "F", "S"];
-      context.fillRect(700+dow.key*10+3, y - val, 9, val);
-      drawLegendSmall(dows[dow.key], 700+dow.key*10+3, y + 8);
-    });
-    summaries[3].forEach(function (hour) {
-      context.fillStyle = legendColor;
-      var val = hour.value / (factor / 2);
-      context.fillRect(800+hour.key*10+3, y - val, 9, val);
-      drawLegendSmall(""+(hour.key), 800+hour.key*10+3, y + 8);
-    });
-  }
-
-  function drawAnnotations() {
-    for (var i in window.annotations) {
-      var annotation = window.annotations[i];
-
-      var days = daysSinceStart(annotation.date);
-      var pos  = coordForTime(days, 0, 600);
-
-      context.strokeStyle = "#3333ff";
-      context.fillStyle = "#3333ff";
-      context.lineWidth = 0.5;
-      context.globalAlpha = 0.5;
-      context.beginPath();
-      context.moveTo(pos[0] + 2, pos[1] + annotation.pos * 12);
-      context.lineTo(pos[0] + 2, 30);
-      context.stroke();
-      context.globalAlpha = 1.0;
-      context.font = "10px Helvetica";
-      context.fillText(annotation.text, pos[0] + 5, pos[1] + annotation.pos * 12);
-    }
-  }
-
-  function drawCommits() {
-    var x, y, i, j;
-    var days, pos;
-    var workTimeline = 160;
-
-    clear(workTimeline);
-
-    drawHeader("Commits", 10, workTimeline - 120);
-    context.beginPath();
-    drawGrid(0, workTimeline);
-
-    var colors = ["#ff6600", "#0000ff", "#ff0000", "#aaaaaa"];
-    var lastCompany = "";
-    var colorIndex = -1;
-
-    window.commits.forEach(function(commit) {
-      var days = daysSinceStart(commit.date);
-      var pos  = coordForTime(days, commit.date.getHours(), workTimeline);
-      if (lastCompany !== String(commit.company)) {
-        lastCompany = String(commit.company);
-        colorIndex++;
-        context.fillStyle = colors[colorIndex];
-      }
-
-      context.fillRect(pos[0]-day_w/2, pos[1]-hour_h/2, day_w, hour_h);
-    });
-
-    var summaries = [
+  function drawCommits(groups, filters) {
+    var chart = new Chart("commit", "Commits");
+    chart.draw(window.commits, groups, filters);
+    chart.drawSummaries([
       window.commitsPerMonth.all(),
       window.commitsPerDay.all(),
       window.commitsPerDOW.all(),
-      window.commitsPerHour.all()
-    ];
-    drawSummaries(summaries, 30, 100);
-  }
-  function drawEmails() {
-    var x, y, i, j;
-    var days, pos;
-    var emailTimeline = 360;
-
-    clear(emailTimeline);
-
-    drawHeader("Emails", 10, emailTimeline - 120);
-    context.beginPath();
-    drawGrid(0, emailTimeline);
-
-    var colors = ["#ff6600", "#0000ff", "#ff0000", "#aaaaaa"];
-    var lastAccount = "";
-    var colorIndex = -1;
-
-    window.emails.forEach(function(email) {
-      var days = daysSinceStart(email.date);
-      var pos  = coordForTime(days, email.date.getHours(), emailTimeline);
-      if (lastAccount !== String(email.account)) {
-        lastAccount = String(email.account);
-        colorIndex++;
-        context.fillStyle = colors[colorIndex];
-      }
-
-      context.fillRect(pos[0]-day_w/2, pos[1]-hour_h/2, day_w, hour_h);
+      window.commitsPerHour.all(),
+    ], 100);
+    chart.drawSummary(window.commitsPerLanguage.all(), 1084, 100, 35, function(x) {
+      var map = { "javascript": "js", "coffeescript": "coffee", "objective-c": "obj-c" }
+      return((map[x.key] || x.key).toUpperCase());
     });
+    chart.drawAnnotations();
+  }
 
-    var summaries = [
+  function drawEmails() {
+    var chart = new Chart("email", "Email sent");
+    chart.draw(window.emails, "account");
+    chart.drawSummaries([
       window.emailsPerMonth.all(),
       window.emailsPerDay.all(),
       window.emailsPerDOW.all(),
-      window.emailsPerHour.all()
-    ];
-    drawSummaries(summaries, 230, 10);
-
+      window.emailsPerHour.all(),
+    ], 30);
+    chart.drawAnnotations();
   }
+
   function drawSocial() {
-    var x, y, i, j;
-    var days, pos;
-    var socialTimeline = 560;
-
-    clear(socialTimeline);
-
-    drawHeader("Social Media", 10, socialTimeline - 120);
-
-    context.beginPath();
-    drawGrid(0, socialTimeline);
-
-    var colors = ["#9ae4e8", "#3b5998"];
-    var lastAccount = "";
-    var colorIndex = -1;
-
-    window.social.forEach(function(post) {
-      var days = daysSinceStart(post.date);
-      var pos  = coordForTime(days, post.date.getHours(), socialTimeline);
-
-      if (lastAccount !== String(post.service)) {
-        lastAccount = String(post.service);
-        colorIndex++;
-        context.fillStyle = colors[colorIndex];
-      }
-
-      context.fillRect(pos[0]-day_w/2, pos[1]-hour_h/2, day_w, hour_h);
-    });
-
-    var summaries = [
+    var chart = new Chart("social", "Social");
+    chart.colors = ["#3b5998", "#9ae4e8"];
+    chart.draw(window.social, "service");
+    chart.drawSummaries([
       window.postsPerMonth.all(),
       window.postsPerDay.all(),
       window.postsPerDOW.all(),
-      window.postsPerHour.all()
-    ];
-    drawSummaries(summaries, 430, 10);
+      window.postsPerHour.all(),
+    ], 30);
+    chart.drawAnnotations();
   }
 
+  function drawIM() {
+    var chart = new Chart("im", "IM");
+    chart.draw(window.ims, "service");
+    chart.drawSummaries([
+      window.imPerMonth.all(),
+      window.imPerDay.all(),
+      window.imPerDOW.all(),
+      window.imPerHour.all(),
+    ], 1000);
+    chart.drawAnnotations();
+  }
+  //{{{ - Data loading
   function loadAnnotations() {
-    d3.csv("annotation-data.csv", function(posts) {
+    d3.csv("http://rody.dev/data/annotations", function(posts) {
       posts.forEach(function(post, index) {
         post.index = index;
         post.date  = new Date(post.date);
       });
       window.annotations = posts;
 
-      drawAnnotations();
+      loadData();
     });
   }
-  //}}}
 
-  context.fillText("Loading commits...", 10, 10);
-  context.fillText("Loading emails...",  10, 240);
-  context.fillText("Loading social...",  10, 480);
+  function loadData() {
+    d3.csv("http://rody.dev/data/commits", function(commits) {
+      commits.forEach(function(commit, index) {
+        commit.index = index;
+        commit.date  = new Date(commit.date);
+      });
 
-  d3.csv("commit-data.csv", function(commits) {
-    commits.forEach(function(commit, index) {
-      commit.index = index;
-      commit.date  = new Date(commit.date);
+      var identity = function(x) { return x; };
+      var cCommits = crossfilter(commits);
+      var commitsByMonth  = cCommits.dimension(function(commit) { return commit.date.getMonth(); });
+      var commitsPerMonth = commitsByMonth.group(identity);
+      var commitsByDay    = cCommits.dimension(function(commit) { return commit.date.getDate(); });
+      var commitsPerDay   = commitsByDay.group(identity);
+      var commitsByDOW    = cCommits.dimension(function(commit) { return commit.date.getDay(); });
+      var commitsPerDOW   = commitsByDOW.group(identity);
+      var commitsByHour   = cCommits.dimension(function(commit) { return commit.date.getHours(); });
+      var commitsPerHour  = commitsByHour.group(identity);
+      var commitsByLanguage  = cCommits.dimension(function(commit) { return commit.language; });
+      var commitsPerLanguage = commitsByLanguage.group(identity);
+      window.commits = commits;
+      window.commitsPerMonth = commitsPerMonth;
+      window.commitsPerDay = commitsPerDay;
+      window.commitsPerDOW = commitsPerDOW;
+      window.commitsPerHour = commitsPerHour;
+      window.commitsPerLanguage = commitsPerLanguage;
+      drawCommits();
     });
+    d3.csv("http://rody.dev/data/emails", function(emails) {
+      emails.forEach(function(email, index) {
+        email.index = index;
+        email.date  = new Date(email.date);
+      });
 
-    var cCommits = crossfilter(commits);
-    var commitsByMonth  = cCommits.dimension(function(commit) { return commit.date.getMonth(); });
-    var commitsPerMonth = commitsByMonth.group(function(c) { return c; });
-    var commitsByDay    = cCommits.dimension(function(commit) { return commit.date.getDate(); });
-    var commitsPerDay   = commitsByDay.group(function(c) { return c; });
-    var commitsByDOW    = cCommits.dimension(function(commit) { return commit.date.getDay(); });
-    var commitsPerDOW   = commitsByDOW.group(function(c) { return c; });
-    var commitsByHour   = cCommits.dimension(function(commit) { return commit.date.getHours(); });
-    var commitsPerHour  = commitsByHour.group(function(c) { return c; });
-    window.commits = commits;
-    window.commitsPerMonth = commitsPerMonth;
-    window.commitsPerDay = commitsPerDay;
-    window.commitsPerDOW = commitsPerDOW;
-    window.commitsPerHour = commitsPerHour;
-    drawCommits();
+      var cEmails = crossfilter(emails);
+      var emailsByMonth  = cEmails.dimension(function(email) { return email.date.getMonth(); });
+      var emailsPerMonth = emailsByMonth.group(function(c) { return c; });
+      var emailsByDay    = cEmails.dimension(function(email) { return email.date.getDate(); });
+      var emailsPerDay   = emailsByDay.group(function(c) { return c; });
+      var emailsByDOW    = cEmails.dimension(function(email) { return email.date.getDay(); });
+      var emailsPerDOW   = emailsByDOW.group(function(c) { return c; });
+      var emailsByHour   = cEmails.dimension(function(email) { return email.date.getHours(); });
+      var emailsPerHour  = emailsByHour.group(function(c) { return c; });
 
-    loadAnnotations();
-  });
-  d3.csv("email-data.csv", function(emails) {
-    emails.forEach(function(email, index) {
-      email.index = index;
-      email.date  = new Date(email.date);
+      window.emails = emails;
+      window.emailsPerMonth = emailsPerMonth;
+      window.emailsPerDay = emailsPerDay;
+      window.emailsPerDOW = emailsPerDOW;
+      window.emailsPerHour = emailsPerHour;
+      drawEmails();
     });
+    d3.csv("http://rody.dev/data/social", function(posts) {
+      posts.forEach(function(post, index) {
+        post.index = index;
+        post.date  = new Date(post.date);
+      });
 
-    var cEmails = crossfilter(emails);
-    var emailsByMonth  = cEmails.dimension(function(email) { return email.date.getMonth(); });
-    var emailsPerMonth = emailsByMonth.group(function(c) { return c; });
-    var emailsByDay    = cEmails.dimension(function(email) { return email.date.getDate(); });
-    var emailsPerDay   = emailsByDay.group(function(c) { return c; });
-    var emailsByDOW    = cEmails.dimension(function(email) { return email.date.getDay(); });
-    var emailsPerDOW   = emailsByDOW.group(function(c) { return c; });
-    var emailsByHour   = cEmails.dimension(function(email) { return email.date.getHours(); });
-    var emailsPerHour  = emailsByHour.group(function(c) { return c; });
+      var cSocial = crossfilter(posts);
+      var postsByMonth  = cSocial.dimension(function(post) { return post.date.getMonth(); });
+      var postsPerMonth = postsByMonth.group(function(c) { return c; });
+      var postsByDay    = cSocial.dimension(function(post) { return post.date.getDate(); });
+      var postsPerDay   = postsByDay.group(function(c) { return c; });
+      var postsByDOW    = cSocial.dimension(function(post) { return post.date.getDay(); });
+      var postsPerDOW   = postsByDOW.group(function(c) { return c; });
+      var postsByHour   = cSocial.dimension(function(post) { return post.date.getHours(); });
+      var postsPerHour  = postsByHour.group(function(c) { return c; });
 
-    window.emails = emails;
-    window.emailsPerMonth = emailsPerMonth;
-    window.emailsPerDay = emailsPerDay;
-    window.emailsPerDOW = emailsPerDOW;
-    window.emailsPerHour = emailsPerHour;
-    drawEmails();
-  });
-  d3.csv("social-data.csv", function(posts) {
-    posts.forEach(function(post, index) {
-      post.index = index;
-      post.date  = new Date(post.date);
+      window.postsPerMonth = postsPerMonth;
+      window.postsPerDay = postsPerDay;
+      window.postsPerDOW = postsPerDOW;
+      window.postsPerHour = postsPerHour;
+      window.social = posts;
+      drawSocial();
     });
+    d3.csv("http://rody.dev/data/im", function(posts) {
+      posts.forEach(function(post, index) {
+        post.index = index;
+        post.date  = new Date(post.date);
+      });
 
-    var cSocial = crossfilter(posts);
-    var postsByMonth  = cSocial.dimension(function(post) { return post.date.getMonth(); });
-    var postsPerMonth = postsByMonth.group(function(c) { return c; });
-    var postsByDay    = cSocial.dimension(function(post) { return post.date.getDate(); });
-    var postsPerDay   = postsByDay.group(function(c) { return c; });
-    var postsByDOW    = cSocial.dimension(function(post) { return post.date.getDay(); });
-    var postsPerDOW   = postsByDOW.group(function(c) { return c; });
-    var postsByHour   = cSocial.dimension(function(post) { return post.date.getHours(); });
-    var postsPerHour  = postsByHour.group(function(c) { return c; });
+      var cIM = crossfilter(posts);
+      var imByMonth  = cIM.dimension(function(post) { return post.date.getMonth(); });
+      var imPerMonth = imByMonth.group(function(c) { return c; });
+      var imByDay    = cIM.dimension(function(post) { return post.date.getDate(); });
+      var imPerDay   = imByDay.group(function(c) { return c; });
+      var imByDOW    = cIM.dimension(function(post) { return post.date.getDay(); });
+      var imPerDOW   = imByDOW.group(function(c) { return c; });
+      var imByHour   = cIM.dimension(function(post) { return post.date.getHours(); });
+      var imPerHour  = imByHour.group(function(c) { return c; });
 
-    window.postsPerMonth = postsPerMonth;
-    window.postsPerDay = postsPerDay;
-    window.postsPerDOW = postsPerDOW;
-    window.postsPerHour = postsPerHour;
-    window.social = posts;
-    drawSocial();
-  });
+      window.imPerMonth = imPerMonth;
+      window.imPerDay = imPerDay;
+      window.imPerDOW = imPerDOW;
+      window.imPerHour = imPerHour;
+      window.ims = posts;
+      drawIM();
+    });
+  }
+
+  loadAnnotations();
+  document.getElementById("refresh").onclick = function(e) {
+    var groups = null;
+    var filter = null;
+    var radios = document.getElementsByName("color[]");
+    for (var i in radios) {
+      if (radios[i].checked) {
+        groups = radios[i].value;
+      }
+    }
+    var companySelect = document.getElementById("company-filter");
+    var company = companySelect.value;
+
+    console.log(groups, company);
+    if (groups)
+      drawCommits(groups, {company: company} );
+  }
 }
 
---
+++ drawing.js
@@ -0,0 +1,231 @@
+var Settings = {
+  startYear: 2006,
+  startMonth: 9,
+  endYear: 2012,
+  width: 12000,
+  height: 160,
+
+  day_w: 4,
+  hour_h: 4,
+  day_gap: 1,
+  hour_gap: 1,
+
+  dows: ["S", "M", "T", "W", "T", "F", "S"],
+  moys: ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+
+  legendColor: "#ccc",
+};
+
+var Chart = function(name, title) {
+  this.name    = name;
+  this.title   = title;
+  this.canvas  = document.getElementById(name + "-canvas");
+  this.context = this.canvas.getContext('2d');
+
+  this.context.fillText("Loading...", 10, 10);
+  this.colors  = [
+    "#00f",
+    "#f60",
+    "#3f3",
+    "#aaa",
+
+    "#33E6D9",
+    "#FFA600",
+    "#A64B00",
+    "#8CCCF2",
+    "#ED0000",
+    "#A6FF00",
+    "#8C19A3",
+    "#00AAE6",
+    "#5CF22C",
+    "#FF6600",
+    "#806600",
+    "#0057D9",
+  ]
+
+  this.daysSinceStart = function(date) {
+    var startDate = new Date("" + Settings.startYear + "-" + Settings.startMonth + "-01 00:00:00");
+    var days = parseInt((date - startDate) / 86400000);
+
+    return(days);
+  };
+
+  this.coordForTime = function (days, hour, y) {
+    return [ days*(Settings.day_w + Settings.day_gap), y - hour*(Settings.hour_h + Settings.hour_gap) ];
+  };
+
+  this.hourLine = function(hour) {
+    this.context.moveTo(0, 160 - hour*(Settings.hour_h + Settings.hour_gap));
+    this.context.lineTo(Settings.width, 160 - hour*(Settings.hour_h + Settings.hour_gap));
+  };
+
+  this.dayLine = function(day, text, offset) {
+    if (typeof(day) === "string") {
+      day = new Date(day);
+    }
+    var days = this.daysSinceStart(day);
+
+    var pos  = this.coordForTime(days, 0, 160);
+    if (typeof(text) === "string") {
+      this.context.font = "9px Helvetica";
+      this.context.fillText(text, pos[0], pos[1] + offset);
+    }
+
+    this.context.moveTo(pos[0], pos[1]);
+    pos  = this.coordForTime(days, 23, 160);
+    this.context.lineTo(pos[0], pos[1]);
+  };
+
+
+  this.clear = function() {
+    this.context.fillStyle = "#ffffff";
+    this.context.fillRect(0, 0, Settings.width, 240);
+  };
+
+  this.drawGrid = function() {
+    this.context.stokeStyle = "#ff0000";
+    this.context.fillStyle  = "#000000";
+    this.context.lineWidth = 0.1;
+    for(i=0;i<24;i++) {
+      this.hourLine(i);
+    }
+
+    for(i=Settings.startYear;i<Settings.endYear+1;i++) {
+      this.dayLine(""+(i) + "-01-01 00:00:00", ""+(i), 20);
+
+      for(j=1;j<13;j++) {
+        this.dayLine("" + (i) + "-" + j + "-01 00:00:00", ""+(j), 30);
+      }
+    }
+
+    // Custom hour markers
+    this.hourLine(9);
+    this.hourLine(12);
+    this.hourLine(18);
+    this.hourLine(20);
+
+    this.context.stroke();
+  };
+
+  this.drawHeader = function(text) {
+    var font = this.context.font;
+    this.context.fillStyle = "#000000";
+    this.context.font = "32px Helvetica";
+    this.context.fillText(text, 10, 40);
+    this.context.font = font;
+  }
+
+  this.drawLegendSmall = function(text, x, y) {
+    this.context.fillStyle = "#000000";
+    this.context.font = "8px Helvetica";
+    this.context.fillText(text, x, y);
+  }
+
+  this.drawSummary = function(values, x, factor, width, legend) {
+    if (typeof(width) === "undefined") {
+      width = 10;
+    }
+    if (typeof(legend) === "undefined") {
+      legend = function(item) { return(item.key); };
+    }
+
+    var i = 0;
+    var self = this;
+    values.forEach(function (item) {
+      self.context.fillStyle = Settings.legendColor;
+      var val = item.value / factor;
+      self.context.fillRect(x+i*width+3, 30 - val, width-1, val);
+      self.drawLegendSmall(legend(item), x + i*width+3, 30 + 8);
+      i++;
+    });
+  };
+
+  this.draw = function(values, group, filters) {
+    var i, j;
+    var days, pos;
+    var legend = document.getElementById("color-legend-" + this.name);
+    legend.innerHTML = "";
+
+    if (typeof(group) === "undefined") {
+      group = "company";
+    }
+
+    this.clear();
+
+    this.drawHeader(this.title);
+    this.context.beginPath();
+    this.drawGrid();
+
+    var lastGroup = "";
+    var colorIndex = -1;
+
+    var sorter = function(a, b) {
+      var ret = a[group] == b[group] ? 0 : ( a[group] < b[group] ? -1 : 1 );
+      return ret;
+    };
+    var self = this;
+
+    self.context.globalAlpha = 0.5;
+    values.sort(sorter).forEach(function(value) {
+      var days = self.daysSinceStart(value.date);
+      var pos  = self.coordForTime(days, value.date.getHours(), 160);
+      if (lastGroup !== String(value[group])) {
+        lastGroup = String(value[group]);
+        colorIndex++;
+        if (colorIndex > self.colors.length - 1) { colorIndex = 0; }
+        self.context.fillStyle = self.colors[colorIndex];
+
+        var li = document.createElement("li");
+        li.innerHTML = '<span class="legend" style="background-color: ' + self.colors[colorIndex] + ';">&nbsp;</span>' + lastGroup;
+        legend.appendChild(li);
+      }
+
+      if (typeof(filters) !== "undefined")
+      {
+        var filtered = false;
+        for (var i in filters) {
+          if (filters[i] !== "" && value[i] !== filters[i])
+            filtered = true;
+        }
+        self.context.fillStyle = filtered ? "#ccc" : self.colors[colorIndex];
+      }
+
+      self.context.fillRect(pos[0]-Settings.day_w/2, pos[1]-Settings.hour_h/2, Settings.day_w, Settings.hour_h);
+    });
+    self.context.globalAlpha = 1.0;
+  };
+
+  this.drawSummaries = function(summaries, factor) {
+    this.drawSummary(summaries[0], 200, factor,     10, function (item) { return (Settings.moys[item.key]); });
+    this.drawSummary(summaries[1], 350, factor / 3  );
+    this.drawSummary(summaries[2], 700, factor,     10, function (item) { return (Settings.dows[item.key]); });
+    this.drawSummary(summaries[3], 800, factor / 2  );
+  };
+
+  this.drawAnnotations = function() {
+    for (var i in window.annotations) {
+      var annotation = window.annotations[i];
+
+      if (annotation.category != this.name) {
+        continue;
+      }
+
+      var days = this.daysSinceStart(annotation.date);
+      var pos  = this.coordForTime(days, 0, 200);
+
+      this.context.strokeStyle = "#3333ff";
+      this.context.fillStyle = "#3333ff";
+      this.context.lineWidth = 0.5;
+      this.context.globalAlpha = 0.5;
+      this.context.beginPath();
+      this.context.moveTo(pos[0] + 2, pos[1] + annotation.pos * 12);
+      this.context.lineTo(pos[0] + 2, 40);
+      this.context.stroke();
+      this.context.globalAlpha = 1.0;
+      this.context.font = "10px Helvetica";
+      this.context.fillText(annotation.text, pos[0] + 5, pos[1] + annotation.pos * 12);
+      this.context.strokeStyle = "#000";
+      this.context.fillStyle = "#000";
+    }
+  };
+}
